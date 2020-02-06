8a221154edcfcbcbf1da0c4f4485efe24712aaca
Georgia Gkegka <21800490@bucks.ac.uk>
2020-02-06 15:11:14 +0000

    Add project files.

---
+++ C0453_ClassConsoleApp1.Unit4/Program.cs
@@ -0,0 +1,23 @@
+﻿using C0453_ClassConsoleApp1.Unit4.Unit4;
+using System;
+
+namespace C0453_ClassConsoleApp1.Unit4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            double miles = 12;
+            double feet = 7260;
+
+            Console.WriteLine("Georgia's Distance Converter");
+            DistanceConverter converter = new DistanceConverter();
+            feet = converter.ToFeet(miles);
+            Console.WriteLine(" The no of feet = " + feet);
+
+            miles = converter.ToMiles(7260*3);
+            Console.WriteLine("The no of miles = " + miles);
+            
+        } 
+    }
+}
---
+++ C0453_ClassConsoleApp1.Unit4/Unit4/DistanceConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C0453_ClassConsoleApp1.Unit4.Unit4
+{
+    public class DistanceConverter
+    {
+        public double ToFeet(double miles)
+        {
+            return miles * 1760.00 * 3;
+        }
+
+        public double ToMiles (double feet )
+        {
+            return feet  / 1760.00 * 3;
+        }
+    }
+}
